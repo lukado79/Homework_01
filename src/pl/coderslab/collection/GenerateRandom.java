eaaac2f15e84e55099d3cb49aca4e12346b12d06
Łukasz Izdebski <lukaszizdebski@gazeta.pl>
2018-02-22 23:55:35 +0100

    der haus

--- src/pl/coderslab/collection/GenerateRandom.java
+++ src/pl/coderslab/collection/GenerateRandom.java
@@ -1,12 +1,22 @@
 package pl.coderslab.collection;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 public class GenerateRandom {
 
 	public static void main(String[] args) {
+		Map<Integer, Integer> maps = checkRand(2, 4);
+
+		for (Integer key : maps.keySet()) {
+			for (Integer value : maps.values()) {
+				System.out.println(key + " : " + value);
+			}
+		}
 
 	}
 
@@ -14,20 +24,39 @@ public class GenerateRandom {
 		Map<Integer, Integer> map = new HashMap<>();
 
 		Random ran = new Random();
-		
+		List<Integer> lista = new ArrayList<>();
 		int occur;
-		int value;
+		int value = 0;
 
-		for (int i = 0; i <= amount; i++) {
-			value = ran.nextInt(interval);
+		for (int i = 0; i < amount; i++) {
+			for (int a = 0; a < interval; a++) {
+				value = ran.nextInt(interval);
+				lista.add(value);
+			}
+
+		}
+		System.out.println(lista.toString());
+
+		for (Integer occ : lista) {
+			for (int j = 0; j < lista.size();) {
+				occur = Collections.frequency(lista, occ);
+				map.put(occ, occur);
+				System.out.println(map.toString());
+				return map;
+
+			}
 
 		}
 
 		return map;
-		
-		//stwórz listę z wylosowanymi liczbami
+
+		// stwórz listę z wylosowanymi liczbami
 		// w pętli for z if sprawdź ile razy wystąpi liczba
-		//dodaj do mapy liczbę jako klucz i liczbę wystąpień jako wartość
+		// dodaj do mapy liczbę jako klucz i liczbę wystąpień jako wartość
+
+	}
+
+	static void List() {
 
 	}
 
